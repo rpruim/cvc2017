3f2435c19ce877811da1cc90820e0293e502ed19
RStudio Guest <rstudio317@rstudio.cs.calvin.edu>
2017-06-15 20:52:26 +0000

    added later function

---
+++ R/later.R
@@ -0,0 +1,12 @@
+#' Say see you later
+#' 
+#' This is a function to automate kindness
+#' That is the only description needed
+#' 
+#' @param name The name of a person to be thanked
+#' 
+later <- function(name="Fred"){
+  cat(paste0("See you later, ",name, "!"))
+}
+later()
+
