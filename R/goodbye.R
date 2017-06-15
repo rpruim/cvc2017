ee207bd2cbd534c30faea76777574a0669f602ad
Robin Shields-Cutler <cutler.robin@gmail.com>
2017-06-15 15:08:49 -0500

    Added the goodbye function documentation and other roxygen2 elements.

--- R/goodbye.R
+++ R/goodbye.R
@@ -1,3 +1,14 @@
+#' @title Say Goodbye
+#' 
+#' @description This is a simple function with a difficult execution, unless you are not fond of the individual.
+#' 
+#' @param name The name of a person that is leaving. Perhaps they are taking a vacation
+#' 
+#' @export
+#' @examples
+#' goodbye()
+#' goodbye('Hal')
+
 goodbye = function(name = 'Fred') {
   cat(paste('Goodbye,', name))
 }
