86d6fdd5779f34ae71bab83eeb2330de4e2cd857
RStudio Guest <rstudio317@rstudio.cs.calvin.edu>
2017-06-15 20:54:32 +0000

    fixed description for later function

--- R/later.R
+++ R/later.R
@@ -1,9 +1,9 @@
 #' Say see you later
 #' 
-#' This is a function to automate kindness
-#' That is the only description needed
+#' This is a function to say see you later!
 #' 
-#' @param name The name of a person to be thanked
+#' 
+#' @param name The name of a person to be spoken to
 #' 
 later <- function(name="Fred"){
   cat(paste0("See you later, ",name, "!"))
