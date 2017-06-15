2f5be1a8bbe5f0529b519a67ae3af38236330ac4
Randall Pruim <rpruim@gmail.com>
2017-06-15 20:00:20 +0000

    add basic roxygen stuff for hello

--- R/hello.R
+++ R/hello.R
@@ -1,3 +1,14 @@
+#' Say hello
+#' 
+#' This is a pretty simple function to extend greetings.
+#' 
+#' @param name The name of a person to be used in the greeting.
+#' @export 
+#' @examples 
+#' hello()
+#' hello("Kitty")
+
+
 hello <- function(name = "Fred") {
   cat(paste0("Hello, ", name, "."))
 }
